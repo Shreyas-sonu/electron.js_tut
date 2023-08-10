fb763d88a57bf0fb328ca80264ee54d706c0a8a2
Shreyas <sonureigns007@gmail.com>
2023-08-10 17:15:18 +0530

    electron basics

---
+++ hello_world/main.js
@@ -0,0 +1,24 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const url = require("url");
+
+let win;
+
+const createWindow = () => {
+  win = new BrowserWindow();
+  win.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "index.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+  win.webContents.openDevTools()
+    win.on('closed', () => {
+        win = null;
+    })
+};
+
+app.on('ready', createWindow);
